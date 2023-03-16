0343e6cfa613bcd6c3e2e07e931253b60fa79cd4
Nisargshah18 <nisargshah1852000@gmail.com>
2023-03-16 16:52:24 -0600

    Working OCR and TTS

--- Project Files/main.py
+++ Project Files/main.py
@@ -2,6 +2,8 @@ import pytesseract
 import cv2
 import json
 from google.cloud import texttospeech
+# import spacy
+# nlp = spacy.load('en_core_web_sm')
 
 # with open('braided-horizon-380818-a343e93b3e20.json') as f:
 #     data = json.load(f)
@@ -9,9 +11,28 @@ from google.cloud import texttospeech
 # print(data)
 
 ##intialize a connection to google cloud texttospeech client using json key
+img = cv2.imread('test.jpeg')
+# Convert the image to grayscale
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# Apply thresholding to remove noise
+thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+
+# Apply Gaussian blur to smooth the image
+blur = cv2.GaussianBlur(thresh, (3, 3), 0)
+
+# Apply Canny edge detection to extract edges
+edges = cv2.Canny(blur, 100, 200)
+
+ocrText = pytesseract.image_to_string(edges)
+# doc = nlp(text2)
+# words = [token.text for token in doc if token.is_alpha]
+
 client = texttospeech.TextToSpeechClient.from_service_account_json('braided-horizon-380818-a343e93b3e20.json')
 
-synthesis_input = texttospeech.SynthesisInput(text="Checking if this working correct so far")
+# for i in range(0,len(words)):
+#     print(words[i])
+synthesis_input = texttospeech.SynthesisInput(text=ocrText)
 
 # Build the voice request
 voice = texttospeech.VoiceSelectionParams(
@@ -29,7 +50,7 @@ response = client.synthesize_speech(
     input=synthesis_input, voice=voice, audio_config=audio_config
 )
 
-# The response's audio_content is binary.
+ # The response's audio_content is binary.
 with open("output.mp3", "wb") as out:
     out.write(response.audio_content)
-    print('Audio content written to file "output.mp3"')
+    print('Audio content written to file output.mp3')
